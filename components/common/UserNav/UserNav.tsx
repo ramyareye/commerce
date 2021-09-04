b0b2062e588a131da4dbd5c48970ec57651942ac
Reza Babaei <reza.tizro@gmail.com>
2021-09-04 16:32:16 +0300

    feat: changes for woocommerce (WIP)

--- components/common/UserNav/UserNav.tsx
+++ components/common/UserNav/UserNav.tsx
@@ -17,10 +17,10 @@ interface Props {
 const countItem = (count: number, item: LineItem) => count + item.quantity
 
 const UserNav: FC<Props> = ({ className }) => {
-  const { data } = useCart()
+  // const { data } = useCart()
   const { data: customer } = useCustomer()
   const { toggleSidebar, closeSidebarIfPresent, openModal } = useUI()
-  const itemsCount = data?.lineItems.reduce(countItem, 0) ?? 0
+  // const itemsCount = data?.lineItems.reduce(countItem, 0) ?? 0
 
   return (
     <nav className={cn(s.root, className)}>
@@ -28,7 +28,7 @@ const UserNav: FC<Props> = ({ className }) => {
         {process.env.COMMERCE_CART_ENABLED && (
           <li className={s.item} onClick={toggleSidebar}>
             <Bag />
-            {itemsCount > 0 && <span className={s.bagCount}>{itemsCount}</span>}
+            {/* {itemsCount > 0 && <span className={s.bagCount}>{itemsCount}</span>} */}
           </li>
         )}
         {process.env.COMMERCE_WISHLIST_ENABLED && (
